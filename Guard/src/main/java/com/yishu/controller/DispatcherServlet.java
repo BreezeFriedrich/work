3a76db764dfbc620448fc268f3373d6bdaef5173
BreezeFriedrich <2918959599@qq.com>
2017-05-04 18:55:11 +0800

    2017.05.04-swipe

--- Guard/src/main/java/com/yishu/controller/DispatcherServlet.java
+++ Guard/src/main/java/com/yishu/controller/DispatcherServlet.java
@@ -1,31 +1,36 @@
 package com.yishu.controller;
 
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by admin on 2017/5/3.
  */
 public class DispatcherServlet extends HttpServlet{
 
-    private static WebApplicationContext wc;
-
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        //init spring factory
-         wc = WebApplicationContextUtils.getWebApplicationContext(this.getServletContext());
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+        Logger logger= LoggerFactory.getLogger(DispatcherServlet.class);
+        logger.info("getServletPath():"+request.getServletPath());
+        logger.info("getContextPath():"+request.getContextPath());
+        logger.info("getRequestURI():"+request.getRequestURI());
+        logger.info("getRequestURL():"+request.getRequestURL().toString());
+        logger.info("getQueryString():"+request.getQueryString());
+        String str=request.getRequestURI();
+        String str2=(str.split("/"))[2];
+        String str3=request.getContextPath()+"/"+"WEB-INF"+"/"+"jsp"+"/"+str2;
+        logger.info("request.getRequestDispatcher(str3).forward-->>"+str3);
+        try {
+            request.getRequestDispatcher(str3).forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-    public static WebApplicationContext getWc() {
-            return wc;
-        }
-
-    public static Object getBean(String beanName){
-            return wc.getBean(beanName);
-        }
 }
