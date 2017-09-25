15af09c96d04ba25bab50b85f0f6dbfef18686a1
BreezeFriedrich <2918959599@qq.com>
2017-09-25 18:44:33 +0800

    2017.09.25-work

--- lockWechat/src/main/webapp/WEB-INF/jsp/gateway/gateway_add.jsp
+++ lockWechat/src/main/webapp/WEB-INF/jsp/gateway/gateway_add.jsp
@@ -9,15 +9,16 @@
   Time: 11:45
 --%>
 <%@ page language="java" contentType="text/html;charset=UTF-8" pageEncoding="UTF-8" %>
-    <%
+<%
     String path = request.getContextPath();
     String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
 %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
 <html>
 <head>
     <base href="<%=basePath%>">
-    <title>添加网关</title>
+    <title>gateway-addGateway</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
@@ -32,6 +33,24 @@
         img.auto-zoom-1 {
             width: 1rem;
             height: 1rem;
+            max-width: 100%;
+            max-height: 100%;
+        }
+        img.auto-zoom-2 {
+            width: 2rem;
+            height: 2rem;
+            max-width: 100%;
+            max-height: 100%;
+        }
+        img.auto-zoom-3 {
+            width: 3rem;
+            height: 3rem;
+            max-width: 100%;
+            max-height: 100%;
+        }
+        img.auto-zoom-4 {
+            width: 4rem;
+            height: 4rem;
             max-width: 100%;
             max-height: 100%;
         }
@@ -51,6 +70,9 @@
     </style>
 </head>
 <body>
+<div style="display: none;">
+    <s:property value=""></s:property>
+</div>
 
 <div class="page-group">
     <!-- 单个page ,第一个.page默认被展示,page-current指定第一次进入展示-->
@@ -79,19 +101,103 @@
 
         <!-- 这里是页面内容区 -->
         <div class="content">
-            <div class="list-block cards-list" id="cardList">
+            <div class="content-block-title">网关详情</div>
+            <div class="list-block" id="cardList">
+                <ul id="UL_gatewayProperty">
+                    <!--
+                        <li class="item-content">
+                            <div class="item-inner">
+                                <div class="item-title">网关名称</div>
+                                <div class="item-after">天字号</div>
+                            </div>
+                        </li>
+                        <li class="item-content">
+                            <div class="item-inner">
+                                <div class="item-title">网关地址</div>
+                                <div class="item-after">江苏省.南京市.雨花台区</div>
+                            </div>
+                        </li>
+                        <li class="item-content">
+                            <div class="item-inner">
+                                <div class="item-title">网关备注</div>
+                                <div class="item-after">格林豪泰</div>
+                            </div>
+                        </li>
+                        <li class="item-content">
+                            <div class="item-inner">
+                                <div class="item-title">网关条码</div>
+                                <div class="item-after">888888</div>
+                            </div>
+                        </li>
+                    -->
+                </ul>
+            </div>
+            <div class="list-block">
+                <ul>
+                    <li class="item-content item-link">
+                        <div class="item-media"><i class="icon icon-f7"></i></div>
+                        <div class="item-inner">
+                            <div class="item-title">
+                                <img class="auto-zoom-1" src="img/connectLock.png" alt=""/>
+                                增加关联门锁
+                            </div>
+                            <div class="item-after"></div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="content-block-title">已关联门锁</div>
+            <div class="list-block cards-list">
+                <ul id="UL_theSpecificGateway">
+                    <!--
+                        <li class='card lock' id='lockLists[x].lockCode' style="margin: 0 0.5rem;border: 0.3rem outset rgba(100,100,0,0.5);border-top-width:0.2rem;">
+                            <div class='card-header' style='background-color: #FAF1FC;'>lockLists[x].lockName</div>
+                            <div class='card-content' style='background-color: #EEFFFF;'>
+                                <div class='card-content-inner'>
+                                    <img class='auto-zoom-3' src='img/lock.png' />
+                                </div>
+                            </div>
+                            <div class='card-footer' style='background-color: #F3FAF3;'>
+                                <p style='color: #00B83F;'>lockLists[x].lockStatus</p><a href='#' class='icon icon-down'></a>
+                            </div>
+                        </li>
+                        <li class='card lock' id='lockLists[x].lockCode' style="margin: 0 0.5rem;border: 0.3rem outset rgba(100,100,0,0.5);border-top-width:0.2rem;">
+                            <div class='card-header' style='background-color: #FAF1FC;'>lockLists[x].lockName</div>
+                            <div class='card-content' style='background-color: #EEFFFF;'>
+                                <div class='card-content-inner'>
+                                    <img class='auto-zoom-3' src='img/lock.png' />
+                                </div>
+                            </div>
+                            <div class='card-footer' style='background-color: #F3FAF3;'>
+                                <p style='color: #00B83F;'>lockLists[x].lockStatus</p><a href='#' class='icon icon-down'></a>
+                            </div>
+                        </li>
+                        <li class='card lock' id='lockLists[x].lockCode' style="margin: 0 0.5rem;border: 0.3rem outset rgba(100,100,0,0.5);border-top-width:0.2rem;">
+                            <div class='card-header' style='background-color: #FAF1FC;'>lockLists[x].lockName</div>
+                            <div class='card-content' style='background-color: #EEFFFF;'>
+                                <div class='card-content-inner'>
+                                    <img class='auto-zoom-3' src='img/lock.png' />
+                                </div>
+                            </div>
+                            <div class='card-footer' style='background-color: #F3FAF3;'>
+                                <p style='color: #00B83F;'>lockLists[x].lockStatus</p><a href='#' class='icon icon-down'></a>
+                            </div>
+                        </li>
+                    -->
+                </ul>
             </div>
         </div>
     </div>
+</div>
 
-    <!-- 其他的单个page内联页（如果有） -->
-    <div class="page">...</div>
+<!-- 其他的单个page内联页（如果有） -->
+<div class="page">...</div>
 </div>
 
 <script type='text/javascript' src='//g.alicdn.com/sj/lib/zepto/zepto.min.js' charset='utf-8'></script>
 <script type='text/javascript' src='//g.alicdn.com/msui/sm/0.6.2/js/sm.min.js' charset='utf-8'></script>
 <script type='text/javascript' src='//g.alicdn.com/msui/sm/0.6.2/js/sm-extend.min.js' charset='utf-8'></script>
-<!--  <script type='text/javascript' src='js/addGateway.js' charset='utf-8'></script>-->
+<script type='text/javascript' src='resources/js/gateway_add.js' charset='utf-8'></script>
 <!-- 默认必须要执行$.init(),实际业务里一般不会在HTML文档里执行，通常是在业务页面代码的最后执行 -->
 <script>
     $(function(){
--- lockWechat/src/main/webapp/resources/js/main.js
+++ lockWechat/src/main/webapp/resources/js/main.js
@@ -2,6 +2,205 @@
  * Copyright (C) 2006-${YEAR} 南京亿数信息科技有限公司 版权所有
  * Nanjing yishu information technology co., LTD. All Rights Reserved.
  */
+var json;
+$(function(){
+    $.ajax({
+        type:"POST",
+        url:"http://localhost:80/lockWechat/device/getDeviceInfo.action",
+        async:false,//设置为同步，即浏览器等待服务器返回数据再执行下一步.
+//	headers:{"Access-Control-Allow-Origin":"*"},
+        data:{"ownerPhoneNumber":"18255683932"},
+//	timeout:3000,
+        dataType:'json',//返回的数据格式：json/xml/html/script/jsonp/text
+
+        success:function(data,status,xhr){
+            json = data;
+        },
+        error:function(xhr,errorType,error){
+            console.log('错误')
+            console.log(xhr)
+            console.log(errorType)
+            console.log(error)
+        },
+//  beforeSend:function(xhr,settins){
+//      console.log(xhr)
+//      console.log('发送前')
+//  },
+//  complete:function(xhr,status){
+//      console.log('结束')
+//  }
+    });
+    showDevices();
+})
+
+function showDevices(){
+//	alert('json : '+json);
+    var UL_gateway=document.createElement('ul');
+    UL_gateway.id="UL_gateway";
+    document.getElementById('cardList').appendChild(UL_gateway);
+    var UL_gateway=document.getElementById('UL_gateway');
+    var UL_lockList=document.createElement('ul');
+    UL_lockList.id="UL_lockList";
+    UL_lockList.style.paddingLeft='0';
+    UL_lockList.style.marginTop='0.5rem';
+    UL_lockList.style.background='rgba(0,0,0,0.3)';
+//	document.getElementById('cardList').appendChild(UL_lockList);
+
+    UL_gateway.innerHTML = createGatewayNode();
+    //事件代理
+    UL_gateway.addEventListener('click',function(ev){
+        var target = ev.target || window.event.srcElement;
+        while(target !== UL_gateway){
+            if(target.getAttribute('class')==='card-content' && target.parentNode.className==='card gateway'){
+                //href内联页面,网关操作
+//      		alert('card-content');
+                //页面跳转并传递参数
+                var gatewayCode=target.parentNode.id
+                window.location.href="addGateway.jsp?gatewayCode="+gatewayCode;
+                //@deprecated 未启用：经struts action跳转
+//      		window.location.href="http://localhost:80/lockWehat/redirect/specificGateway?ownerPhoneNumber=18255683932&gatewayCode=777777";
+                //@deprecated 路由跳转无法加载js
+//      		$.router.load('addGateway.jsp',true);
+                break;
+            }
+            if(target.className==='card-footer' && target.parentNode.className==='card gateway'){
+                //向下扩展DOM,门锁card
+//      		alert('card-footer');
+                selectedGateway=target.parentNode;
+//      		alert(selectedGateway.id);
+//      		$(target).parent('li').siblings('li').remove();
+
+                /*
+                                //门锁节点是网关节点子节点.
+                                if(selectedGateway.contains(UL_lockList)){
+                                    selectedGateway.removeChild(UL_lockList);
+                                }else{
+                                    UL_lockList.innerHTML = createLockNode(selectedGateway.id);
+                                    selectedGateway.appendChild(UL_lockList);
+                                }
+                */
+                //门锁节点与网关节点同级.
+                if(UL_lockList===selectedGateway.nextSibling){
+                    selectedGateway.parentNode.removeChild(UL_lockList);
+                }else{
+                    UL_lockList.innerHTML = createLockNode(selectedGateway.id);
+                    selectedGateway.parentNode.insertBefore(UL_lockList,selectedGateway.nextSibling);
+                }
+                break;
+            }
+//          if(target.tagName.toLowerCase() == 'li'){
+//              console.log('li click~');
+//              break;
+//          }
+            target = target.parentNode;
+        }
+    })
+}
+function createGatewayNode(){
+    var LI_gateway="";
+    for(x in json){
+        LI_gateway += "<li class='card gateway' id='"+json[x].gatewayCode+"' style='border: 0.3rem outset rgba(100,100,0,0.5);'>";
+        LI_gateway += 	"<div class='card-header' style='background-color: #FAF1FC;'>"+json[x].gatewayName+"</div>";
+        LI_gateway += 	"<div class='card-content' style='background-color: #EEFFFF;'>";
+        LI_gateway += 		"<div class='card-content-inner'>";
+        LI_gateway += 			"<img class='auto-zoom-4' src='img/gateway.png' />";
+        LI_gateway += 		"</div>";
+        LI_gateway += 	"</div>";
+        LI_gateway += 	"<div class='card-footer' style='background-color: #F3FAF3;'>";
+        LI_gateway += 		"<p style='color: #00B83F;'>"+json[x].gatewayStatus+"</p><a href='#' class='icon icon-down'></a>";
+        LI_gateway += 	"</div>";
+        LI_gateway += "</li>"+"<br/>";
+    }
+    return LI_gateway;
+}
+
+function createLockNode(gatewayCode){
+//	var LI_lock="
+//		<li class="card">
+//			<div class="card-header" style="background-color: #FAF1FC;">芷蕴小筑</div>
+//			<div class="card-content" style="background-color: #EEFFFF;">
+//				<div class="card-content-inner">
+//					<img class="auto-zoom-5" src="img/lock.png" />
+//				</div>
+//			</div>
+//			<div class="card-footer" style="background-color: #F3FAF3;">
+//				<p style="color: #00B83F;">工作正常</p><a href="#" class="icon icon-down"></a>
+//			</div>
+//		</li>
+//		";
+    var LI_lock="";
+    var lockLists=null;
+    for(x in json){
+        if(json[x].gatewayCode===gatewayCode){
+            lockLists=json[x].lockLists;
+            break;
+        }
+    }
+    for(x in lockLists){
+        LI_lock += "<li class='card lock' id='"+lockLists[x].lockCode+"' style='margin: 0 0.5rem;border: 0.3rem outset rgba(100,100,0,0.5);border-top-width:'0.2rem';'>";
+        LI_lock += 	"<div class='card-header' style='background-color: #FAF1FC;'>"+lockLists[x].lockName+"</div>";
+        LI_lock += 	"<div class='card-content' style='background-color: #EEFFFF;'>";
+        LI_lock += 		"<div class='card-content-inner'>";
+        LI_lock += 			"<img class='auto-zoom-3' src='img/lock.png' />";
+        LI_lock += 		"</div>";
+        LI_lock += 	"</div>";
+        LI_lock += 	"<div class='card-footer' style='background-color: #F3FAF3;'>";
+        LI_lock += 		"<p style='color: #00B83F;'>"+lockLists[x].lockStatus+"</p><a href='#' class='icon icon-down'></a>";
+        LI_lock += 	"</div>";
+        LI_lock += "</li>";
+
+//		LI_lock +=	"<li class='lock' id='"+lockLists[x].lockCode+"' style='border: 0.2rem outset green;'>";
+//		LI_lock +=	"<div class='row no-gutter'>";
+//		LI_lock +=		"<div class='col-50'>"+"<img class='auto-zoom-5' src='img/lock.png' />"+"</div>";
+//		LI_lock +=		"<div class='col-50'>";
+//		LI_lock +=			"<div><p style='margin:0.2rem 0;font-size: 0.8rem;color: red;'>"+lockLists[x].lockName+"</div>";
+//		LI_lock +=			"<div>"+lockLists[x].lockComment+"<br/>"+"</div>";
+//		LI_lock +=			"<div>"+lockLists[x].lockLocation+"<br/>"+"</div>";
+//		LI_lock +=			"<div><p style='margin:0.2rem 0;font-size: 0.8rem;color: red;'>"+lockLists[x].lockStatus+"</div>";
+//		LI_lock +=		"</div>";
+//		LI_lock +=	"</div>";
+//		LI_lock +=	"</li>"+"<br/>";
+
+//		"++" <p>lockLists[x].lockComment</p><p>lockLists[x].lockLocation</p><p>lockLists[x].lockStatus</p>
+    }
+    return LI_lock;
+}
+
+function removeElementsByClass(className){
+    var elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+/*
+	//为了解决: Zepto.js touch模块与手机自带的滑动效果冲突
+	document.addEventListener('touchmove', function (event) {
+				event.preventDefault();
+			}, false);
+*/
+/*
+//	json=eval("("+json+")");
+//	json=JSON.parse(json);
+//	alert(json);
+//	alert(json[0].gatewayName+','+json[0].gatewayComment+','+json[0].gatewayLocation);
+ */
+/*
+//	document.addEventListener('DOMContentLoaded',showDevices());
+//	$('body').on('onload',showDevices());
+//document.getElementById('UL_gateway').onload=showDevices;
+//window.onload=showDevices;
+ */
+/*
+if(UL_lockList.innerHTML === ""){
+	UL_lockList.innerHTML += createLockNode(json,selectedGateway.id);
+//	document.getElementById('UL_gateway').insertBefore(UL_lockList,selectedGateway.nextSibling);
+	selectedGateway.appendChild(UL_lockList);
+}else{
+//	removeElementsByClass("lock");
+	UL_lockList.innerHTML = "";
+}
+*/
 
 var json;
 $(function(){
