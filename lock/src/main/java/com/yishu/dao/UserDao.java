920b9517e033e8af968d7b2353f0a488510f4894
BreezeFriedrich <2918959599@qq.com>
2017-12-20 18:36:43 +0800

    2017-12-20-work

---
+++ lock/src/main/java/com/yishu/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.yishu.dao;
+
+/**
+ * @author <a href="http://www.yishutech.com">Nanjing yishu information technology co., LTD</a>
+ * @version 1.0.0.0 2017-12-20 16:58 admin
+ * @since JDK1.7
+ */
+public interface UserDao {
+}
