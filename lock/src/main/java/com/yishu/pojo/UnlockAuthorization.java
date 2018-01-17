1e9c98a03492f76617f2bbc751224843e1701f71
BreezeFriedrich <2918959599@qq.com>
2018-01-17 18:36:28 +0800

    2018.01.17

---
+++ lock/src/main/java/com/yishu/pojo/UnlockAuthorization.java
@@ -0,0 +1,55 @@
+package com.yishu.pojo;
+
+import java.util.List;
+
+/**
+ * @author <a href="http://www.yishutech.com">Nanjing yishu information technology co., LTD</a>
+ * @version 1.0.0.0 2018-01-17 10:27 admin
+ * @since JDK1.7
+ */
+public class UnlockAuth {
+    private List<IdentityCard> unlockIds;
+    private UnlockPwds unlockPwds;
+    private int unlockIdSize;
+    private int unlockPwdSize;
+
+    public List<IdentityCard> getUnlockIds() {
+        return unlockIds;
+    }
+
+    public void setUnlockIds(List<IdentityCard> unlockIds) {
+        this.unlockIds = unlockIds;
+    }
+
+    public UnlockPwds getUnlockPwds() {
+        return unlockPwds;
+    }
+
+    public void setUnlockPwds(UnlockPwds unlockPwds) {
+        this.unlockPwds = unlockPwds;
+    }
+
+    public int getUnlockIdSize() {
+        return unlockIdSize;
+    }
+
+    public void setUnlockIdSize(int unlockIdSize) {
+        this.unlockIdSize = unlockIdSize;
+    }
+
+    public int getUnlockPwdSize() {
+        return unlockPwdSize;
+    }
+
+    public void setUnlockPwdSize(int unlockPwdSize) {
+        this.unlockPwdSize = unlockPwdSize;
+    }
+
+    public UnlockAuth getUnlockAuth(List<IdentityCard> unlockIds,UnlockPwds unlockPwds){
+        this.unlockIds=unlockIds;
+        this.unlockPwds=unlockPwds;
+        this.unlockIdSize=unlockIds.size();
+        this.unlockPwdSize=unlockPwds.getPasswordList().size();
+        return this;
+    }
+}
